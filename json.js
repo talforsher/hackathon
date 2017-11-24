f79cb6c6ea493b2fce1346aad435258785ad2740
beerot <talforsher1@gmail.com>
2017-11-24 08:58:41 +0200

    Update json.js

--- json.js
+++ json.js
@@ -1,5 +1,8 @@
 { 
-  "title": "hackathon israel",
-  "lastName": "Doe",
-  "age": 25
+  "title":"hackathon israel",
+  "age":"30.11.1990"  
+}
+{ 
+  "title":"moshe mizrahi",
+  "age":"1.1.1911"  
 }
