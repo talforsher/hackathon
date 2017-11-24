f347763e6ac00231ab165682415bc919a9e9de6a
beerot <talforsher1@gmail.com>
2017-11-24 10:00:39 +0200

    Update json.js

--- json.js
+++ json.js
@@ -1,4 +1,13 @@
-{ 
-  "title": "hackathon israel",
-  "age": "30.11.1990"  
+{
+  "obj":[
+  {
+    "title": "hackathon israel",
+    "age": "30.11.1990",
+  }
+  ,
+  {
+    "title": "moshe",
+    "age": "11.11.1111",
+  }
+    ]
 }
