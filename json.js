4b506d6522f27ed4c5e3249be7bf32c7e4161de7
beerot <talforsher1@gmail.com>
2017-12-11 09:31:52 +0530

    Update json.js

--- json.js
+++ json.js
@@ -6,8 +6,6 @@ var info = [
 	medi = ["medithon","21/12/2017","//static.wixstatic.com/media/e7205b_4fed61c607ba4f08a128b6a22022548b~mv2.png/v1/fill/w_240,h_106,al_c,usm_0.66_1.00_0.01/e7205b_4fed61c607ba4f08a128b6a22022548b~mv2.png", "//medint.io/medithon"],
 
 	data = ["datathon","28/12/2017","//www.panim.org.il/sites/default/files/logo_%20aguda5.jpg","//www.panim.org.il"],
-	
-	data = ["datathon","28/12/2017","//www.panim.org.il/sites/default/files/logo_%20aguda5.jpg","//www.panim.org.il"],
 
 	mta = ["tel aviv yaffo academic college","01/02/2018","//www.mta.ac.il/_layouts/15/SolBox.MTA.UI/Images/logo.png", "//mta.ac.il"]
 
