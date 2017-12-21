cbc6ec76d06e8a6402cadcf750d6d6a2b3316eb1
beerot <talforsher1@gmail.com>
2017-12-21 08:56:06 +0530

    Update json.js

--- json.js
+++ json.js
@@ -11,6 +11,8 @@ var info = [
 	
 	idf = ["israel defense force" , "21/03/2018" , "https://www.idf.il/media/23378/banner3.jpg" , "//idf.il/אתרים/האקתון-פורצים-גבולות/"],
 	
-	iem = ["shenkar" , "28/03/2018", "https://static.wixstatic.com/media/6ac214_21d36eb6fe9d4284aac83de149c72b7f~mv2.jpg/v1/fill/w_255,h_56,al_c,q_80,usm_0.66_1.00_0.01/6ac214_21d36eb6fe9d4284aac83de149c72b7f~mv2.webp" , "//hackathoniem.co.il"]
+	iem = ["shenkar" , "28/03/2018", "https://static.wixstatic.com/media/6ac214_21d36eb6fe9d4284aac83de149c72b7f~mv2.jpg/v1/fill/w_255,h_56,al_c,q_80,usm_0.66_1.00_0.01/6ac214_21d36eb6fe9d4284aac83de149c72b7f~mv2.webp" , "//hackathoniem.co.il"],
+
+	shatil = ["shatil" , "28/12/2018" , "https://www.shatil.org.il/sites/default/files/styles/original/public/hakatonsmall.jpg" , "//shatil.org.il/node/171758"]
 ];
 }
